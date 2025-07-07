430929f15c97127329240f5ab01219dfe1eb8010
Rick <rbrunstedt@gmail.com>
2025-07-07 11:09:22 +0200

    setup

---
+++ lua/config/set.lua
@@ -0,0 +1,48 @@
+vim.g.mapleader = " "
+vim.opt.relativenumber = true
+vim.o.number = true
+vim.o.numberwidth = 1
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.shiftround = true
+vim.o.mouse = "a"
+vim.o.wrap = true
+vim.o.signcolumn = "yes"
+vim.opt.smartindent = true
+vim.opt.wrap = true
+vim.o.foldlevel = 99
+vim.o.foldmethod = "indent"
+vim.o.foldenable = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+-- vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+vim.opt.undofile = true
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
+vim.opt.autoread = true
+vim.opt.exrc = true
+vim.opt.secure = true
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+  pattern = "*",
+  command = "checktime"
+})
+
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI", "FocusGained" }, {
+  pattern = "*",
+  command = "checktime"
+})
+
+
+-- Handle copy paste between neovim and everyting else
+vim.api.nvim_create_autocmd({ "FocusGained" }, {
+  pattern = { "*" },
+  command = [[call setreg("@", getreg("+"))]],
+})
+vim.api.nvim_create_autocmd({ "FocusLost" }, {
+  pattern = { "*" },
+  command = [[call setreg("+", getreg("@"))]],
+})
