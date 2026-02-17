868c4b78573d20da84289f3974fa9eaf79e4a10d
Rick <rbrunstedt@gmail.com>
2026-02-17 10:48:49 +0100

    always close terminal not toggle when in terminal

--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -3,6 +3,14 @@
 local terminal_buf = nil
 local terminal_win = nil
 
+local function close_terminal()
+  -- Close the terminal window if it exists and is valid
+  if terminal_win and vim.api.nvim_win_is_valid(terminal_win) then
+    vim.api.nvim_win_close(terminal_win, false)
+    terminal_win = nil
+  end
+end
+
 local function toggle_terminal()
   -- Check if terminal window exists and is valid
   if terminal_win and vim.api.nvim_win_is_valid(terminal_win) then
@@ -48,40 +56,5 @@ end
 -- Bind Ctrl+/ (which appears as Ctrl+_) to toggle terminal
 vim.keymap.set({"n", "t"}, "<C-_>", toggle_terminal, { desc = "Toggle terminal", silent = true })
 vim.keymap.set({"n", "t"}, "<C-/>", toggle_terminal, { desc = "Toggle terminal", silent = true })
-vim.keymap.set({"t"}, "<esc><esc>", toggle_terminal, { desc = "Toggle terminal", silent = true })
+vim.keymap.set({"t"}, "<esc><esc>", close_terminal, { desc = "Close terminal", silent = true })
 
-
--- Old setup
--- local set = vim.opt_local
---
--- -- Set local settings for terminal buffers
--- vim.api.nvim_create_autocmd("TermOpen", {
--- 	group = vim.api.nvim_create_augroup("custom-term-open", {}),
--- 	callback = function()
--- 		set.number = false
--- 		set.relativenumber = false
--- 		set.scrolloff = 0
---
--- 		vim.bo.filetype = "terminal"
--- 	end,
--- })
---
--- -- Easily hit escape in terminal mode.
--- vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
-
--- open a terminal at the bottom of the screen with a fixed height.
--- vim.keymap.set("n", ",st", function()
--- 	vim.cmd.new()
--- 	vim.cmd.wincmd("j")
--- 	vim.api.nvim_win_set_height(0, 12)
--- 	vim.wo.winfixheight = true
--- 	vim.cmd.term()
--- end, { desc = "Open Terminal" })
-
--- vim.keymap.set("n", "t", function()
--- 	vim.cmd.new()
--- 	vim.cmd.wincmd("j")
--- 	vim.api.nvim_win_set_height(0, 12)
--- 	vim.wo.winfixheight = true
--- 	vim.cmd.term()
--- end, { desc = "Open Terminal" })
